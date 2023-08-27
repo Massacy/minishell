fd1391454a0c2515cc7db3e5b87b2372e37e6010
Massacy <mayoi.dev2226@gmail.com>
2023-08-28 06:43:39 +0900

    echo 対応(仮)

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: imasayos <imasayos@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 06:01:01 by imasayos          #+#    #+#             */
-/*   Updated: 2023/08/28 06:01:02 by imasayos         ###   ########.fr       */
+/*   Updated: 2023/08/28 06:40:54 by imasayos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+int free_split(char **split)
+{
+	int i;
+
+	i = 0;
+	while (split[i] != NULL)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (0);
+}
+
 int main(int argc, char const *argv[])
 {
 	char *prompt;
+	char **split;
 
 	(void)argc;
 	(void)argv;
@@ -26,11 +41,28 @@ int main(int argc, char const *argv[])
 		prompt = readline("$> ");
 		if (prompt == NULL)
 			break;
-		if (ft_strncmp(prompt, "exit", 4) == 0)
+		if (ft_strlen(prompt) == 0)
+			continue;
+		split = ft_split(prompt, ' ');
+		add_history(prompt);
+		if (ft_strlen(prompt) == 4 && ft_strncmp(prompt, "exit", 4) == 0)
+		{
+			free(prompt);
+			free_split(split);
 			break;
-		printf("%s\n", prompt);
+		}
+		else if (ft_strlen(split[0]) == 4 && ft_strncmp(split[0], "echo", 4) == 0)
+		{
+			if (split[1] == NULL)
+				printf("\n");
+			else
+				printf("%s\n", &prompt[5]);
+		}
+		else
+			printf("builtin not found: %s\n", prompt);
 		free(prompt);
+		free_split(split);
 	}
-	printf("exit\n");
+	printf("bye~!\n");
 	return 0;
 }
