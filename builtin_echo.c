a41ac331a0ffad3ad7b2087d3b6c6f85c913c359
Massacy <mayoi.dev2226@gmail.com>
2023-11-23 04:13:36 +0900

    norm fix builtin_echo.c

--- builtin_echo.c
+++ builtin_echo.c
@@ -6,16 +6,16 @@
 /*   By: imasayos <imasayos@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 17:55:47 by imasayos          #+#    #+#             */
-/*   Updated: 2023/11/23 02:25:19 by imasayos         ###   ########.fr       */
+/*   Updated: 2023/11/23 04:12:56 by imasayos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool is_n_option(char *s)
+bool	is_n_option(char *s)
 {
 	int		i;
-	bool 	nflg;
+	bool	nflg;
 
 	nflg = false;
 	i = 0;
@@ -39,10 +39,10 @@ int	builtin_echo(char **argv)
 	i = 1;
 	while (argv[i] != NULL && argv[i][0] == '-')
 	{
-		if(is_n_option(argv[i]+1))
+		if (is_n_option(argv[i] + 1))
 			nflg = true;
 		else
-			break;
+			break ;
 		i++;
 	}
 	while (argv[i] != NULL)
